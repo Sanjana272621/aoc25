ca6a32fe460f3ba7244e38e33101ad09900f548f
Sanjana <sanjana.sureshkumar05@gmail.com>
2025-12-20 10:28:13 +0530

    cafeteria part 1

--- day_5/cafeteria.py
+++ day_5/cafeteria.py
@@ -1,5 +1,6 @@
 def cafeteria():
     with open("day5_input.txt", "r") as f:
+    #with open("sample.txt", "r") as f:
         input_list = f.read().splitlines()
         fresh_count = 0
         fresh_end, id_beg = 0, 0
@@ -12,11 +13,19 @@ def cafeteria():
         fresh = input_list[:fresh_end]
         available = input_list[id_beg:]
 
-        for i in fresh:
-            print(i)
-        print(available)
+        for id in available:
+            break_all = False 
+            for i in fresh:
+                start, end = i.split("-")
+                if int(id) in range(int(start), int(end)+1):
+                    fresh_count += 1 
+                    break_all = True 
+                    break 
+                
+                if break_all:
+                    break
+        
         return fresh_count
-    
 
 if __name__ == "__main__":
     print(cafeteria())
